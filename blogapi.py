aeb9dd86cf4b40a19934a34eb4db47fa9bff17e5
Ryan Saul <seccom@gmail.com>
2017-02-08 00:50:56 -0800

    get all posts from db now working, hooked up request content to writing to db, various refactoring

--- blogapi.py
+++ blogapi.py
@@ -1,5 +1,5 @@
 # Blog API
-
+import json
 import sqlite3
 
 from flask import Flask, request
@@ -7,12 +7,12 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-# TODO: add tests
-
-
-def get_latest_post_id(cursor):
+def get_next_post_id(cursor):
     cursor.execute("SELECT MAX(post_id) FROM posts")
-    post_id = cursor.fetchone()[0] + 1
+    try:
+        post_id = cursor.fetchone()[0] + 1
+    except Exception:
+        post_id = 1
     print("Next post_id: {}".format(post_id))
 
     return post_id
@@ -30,45 +30,54 @@ def create_connection(db_file='blog.db'):
     return None
 
 
+def add_post_to_database(body, title):
+    connection = create_connection()
+    cursor = connection.cursor()
+    post_id = get_next_post_id(cursor)
+    sql_command = '''
+        INSERT INTO posts(post_id, title, body)
+        VALUES ({0}, "{1}", "{2}")
+        '''.format(post_id, title, body)
+    print('SQL COMMAND: {}'.format(sql_command))
+    cursor.execute(sql_command)
+    connection.commit()
+    connection.close()
+
+
 @app.route("/post", methods=['POST'])
-def create_post(title='default_title', body='default_body'):
-    # TODO: add parameters to request in JSON
-    if request.method == 'POST':
-        connection = create_connection()
-        cursor = connection.cursor()
-
-        post_id = get_latest_post_id(cursor)
-
-        sql_command = '''
-                INSERT INTO posts(post_id, title, body)
-                VALUES ({0}, "{1}", "{2}")
-                '''.format(post_id, title, body)
-
-        cursor.execute(sql_command)
-
-        connection.commit()
-        connection.close()
-    else:
-        # TODO: add 400 error here: can only use POST
-        pass
+def create_post():
+    content = request.json
+    print('Incoming Request {}'.format(content))
+    try:
+        title = content['title']
+        body = content['body']
+        add_post_to_database(body, title)
+        return '', 201
+    except Exception as e:
+        print('ERROR: {}'.format(e))
+        return 'Error while processing request, check the body of your request', 400
 
 
 @app.route("/posts")
 def get_all_posts():
-    #TODO: return as JSON-formated
     connection = create_connection()
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM posts")
+    cursor.execute("SELECT post_id, title, body FROM posts")
+    print('Posts retrieved from database')
 
     posts = cursor.fetchall()
 
+    response_content = []
     for post in posts:
-        print(post)
-
-
-def hello():
-    return "Hello World!"
+        response_content.append(
+            {
+                'post_id': post[0],
+                'title': post[1],
+                'body': post[2]
+            }
+        )
+    return json.dumps(response_content), 200
 
 
 if __name__ == "__main__":
